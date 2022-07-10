4c5d878eda5777cf3ae9eb1bd1aa16fc48d03e3f
Andras Schaffer <eosbandi@gmail.com>
2022-07-10 22:54:55 +0200

    Initial commit

---
+++ BatteryBarPlugin.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using MissionPlanner.Utilities;
+using System.IO;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using MissionPlanner.Maps;
+using System.Collections.Generic;
+using System.Net;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using MissionPlanner.Controls;
+
+namespace MissionPlanner.Maps
+{
+
+    public class GMapMarkerWeather : GMapMarker
+    {
+        Bitmap weather;
+        private RectLatLng rect;
+
+        public GMapMarkerWeather(String imageName, RectLatLng rect, PointLatLng currentloc)
+        : base(currentloc)
+        {
+            this.rect = rect;
+            weather?.Dispose();
+            weather = new Bitmap(imageName);
+            weather.MakeTransparent();
+        }
+
+
+        public override void OnRender(IGraphics g)
+        {
+            base.OnRender(g);
+
+            if (weather != null)
+            {
+
+                var tlll = Overlay.Control.FromLatLngToLocal(rect.LocationTopLeft);
+                var brll = Overlay.Control.FromLatLngToLocal(rect.LocationRightBottom);
+
+                var old = g.Transform;
+                g.ResetTransform();
+                g.CompositingMode = CompositingMode.SourceOver;
+                g.DrawImage(weather, tlll.X, tlll.Y, brll.X - tlll.X, brll.Y - tlll.Y);
+                g.Transform = old;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            weather.Dispose();
+
+        }
+
+
+    }
+
+}
+
+
+
+namespace MissionPlanner.BatteryBarPlugin
+{
+
+
+    public class BatteryBarPlugin : MissionPlanner.Plugin.Plugin
+    {
+
+
+        BatteryIndicatorBar bBar;
+        float mahPerMin;
+        float avgSpeed;
+
+        float battCap;
+
+
+
+        public override string Name
+        {
+            get { return "BatteryBar"; }
+        }
+
+        public override string Version
+        {
+            get { return "0.1"; }
+        }
+
+        public override string Author
+        {
+            get { return "Andras Schaffer"; }
+        }
+
+        //[DebuggerHidden]
+        public override bool Init()
+        {
+            loopratehz = 1f;
+
+            ////weather
+
+            MainV2.instance.BeginInvoke((MethodInvoker)(() =>
+            {
+
+               var FDRightSide = Host.MainForm.FlightData.Controls.Find("splitContainer1", true).FirstOrDefault() as SplitContainer;
+                bBar = new BatteryIndicatorBar();
+                bBar.Name = "batteryBar1";
+                bBar.Location = new Point(66, 24);
+                bBar.Size = new Size(Host.FDGMapControl.Width - 66 -6, 15);
+                bBar.Anchor = (AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right);
+                bBar.BringToFront();
+
+                FDRightSide.Panel2.Controls.Add(bBar);
+                FDRightSide.Panel2.Controls.SetChildIndex(bBar, 1);
+
+            }));
+
+            //Get parameters
+            mahPerMin = Host.config.GetFloat("BBAR_MAHPERMIN", 700);
+            Host.config["BBAR_MAHPERMIN"] = mahPerMin.ToString();
+
+            avgSpeed = Host.config.GetFloat("BBAR_AVGSPEED", 10);
+            Host.config["BBAR_AVGSPEED"] = avgSpeed.ToString();
+
+            return true;
+        }
+
+
+        public override bool Loaded()
+        {
+            return true;
+        }
+
+        public override bool Loop()
+        {
+            if (Host.cs.connected)
+            {
+
+                if (battCap == 0)
+                {
+                    if (MainV2.comPort.MAV.param.ContainsKey("BATT_CAPACITY"))
+                    {
+                        battCap = (float)MainV2.comPort.MAV.param["BATT_CAPACITY"].Value;
+                        bBar.BatteryCapacity = battCap;
+                    }
+                }
+                //MahPerMinute flight 
+                bBar.mahPerMinute = mahPerMin;
+                //avgSpeed when coming home (m/s)
+                bBar.avgSpeed = avgSpeed;
+                bBar.UsedMah = (float)Host.cs.battery_usedmah;
+                bBar.DistFromHome = Host.cs.DistToHome;
+            }
+
+            bBar?.Invalidate();
+            return true;
+        }
+
+        public override bool Exit()
+        {
+
+            return true;
+        }
+
+
+        //public void setTimeLabel(string s)
+        //{
+        //    MainV2.instance.BeginInvoke((MethodInvoker)(() =>
+        //    {
+
+        //        weatherImageDate.Text = s;
+        //    }));
+        //}
+
+
+
+
+    }
+
+}
---
+++ BatteryIndicatorBar.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner.BatteryBarPlugin
+{
+    public partial class BatteryIndicatorBar : UserControl
+    {
+
+        private float _batteryCapacity;
+
+        public float BatteryCapacity
+        {
+            get { return _batteryCapacity; }
+            set { _batteryCapacity = value; }
+        }
+
+        private float _usedMah;
+
+        public float UsedMah  
+        {
+            get { return _usedMah; }
+            set { _usedMah = value; }
+        }
+
+        private float _distFromHome;
+
+        public float DistFromHome
+        {
+            get { return _distFromHome; }
+            set { _distFromHome = value; }
+        }
+
+        public float mahPerMinute { get; set; }
+        public float avgSpeed { get; set; }
+
+
+        private readonly Brush _brushbar = new SolidBrush(Color.FromArgb(50, Color.White));
+        private readonly Brush _brushGreen = new SolidBrush(Color.FromArgb(255, Color.ForestGreen));
+        private readonly Brush _brushOrange = new SolidBrush(Color.FromArgb(255, Color.DarkOrange));
+        private readonly Brush _brushRed = new SolidBrush(Color.FromArgb(255, Color.Red));
+
+
+        //Drawing buffer which will be resized to the control size
+        Bitmap buffer = new Bitmap(640, 480);     
+        
+
+
+
+        public BatteryIndicatorBar()
+        {
+            this.DoubleBuffered = false;
+            InitializeComponent();
+        }
+
+        public void DoPaintRemote(PaintEventArgs e)
+        {
+            var matrix = new System.Drawing.Drawing2D.Matrix();
+            matrix.Translate(this.Left, this.Top);
+            e.Graphics.Transform = matrix;
+            OnPaint(e);
+            e.Graphics.ResetTransform();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            //Calculate Safe home battery
+            //
+            // Safe flight time with full battery = 65min
+            // Mah needed for 1 minute flight = FullCapacity / 65min
+            // Estimated speed to home = 10m/s = 600m/min
+            // Mah needed for home = distance / 600 * (Mahfor1min)
+
+            float mahToHome = (_distFromHome / (avgSpeed * 60)) * mahPerMinute;
+
+            using (Graphics etemp = Graphics.FromImage(buffer))
+            {
+                RectangleF bar = new RectangleF(0, 0, this.Width, this.Height);
+
+                etemp.Clear(Color.Transparent);
+                etemp.FillRectangle(_brushbar, bar);
+
+                //Validity check
+                float battPercent = 0;
+                if (_usedMah == 0) battPercent = 1;
+                else if (_usedMah < _batteryCapacity) battPercent = 1- (_usedMah/_batteryCapacity);
+
+                if (_batteryCapacity == 0) battPercent = 0;
+
+                RectangleF bartrav = new RectangleF(bar.X, bar.Y, bar.Width * battPercent, bar.Height);
+
+
+
+
+                if (battPercent < .3)
+                {
+                    etemp.FillRectangle(_brushRed, bartrav);
+                }
+                else if (mahToHome > (_batteryCapacity - _usedMah))
+                {
+                    etemp.FillRectangle(_brushOrange, bartrav);
+                }
+                else
+                {
+                    etemp.FillRectangle(_brushGreen, bartrav);
+                }
+
+                string percent = (battPercent * 100) .ToString("0") + "%";
+                var strSize = etemp.MeasureString(percent, this.Font);
+
+                if (battPercent > 0.8f)
+                {
+                    etemp.DrawString(percent, this.Font, new SolidBrush(this.ForeColor), bartrav.Right-strSize.Width-5,
+                        bartrav.Bottom - FontHeight);
+
+                }
+                else
+                {
+                    etemp.DrawString(percent, this.Font, new SolidBrush(this.ForeColor), bartrav.Right + 5,
+                        bartrav.Bottom - FontHeight);
+                }
+
+                //mahToHome = _batteryCapacity * .5f;
+                etemp.FillPie(Brushes.Yellow, (bar.X + bar.Width * (mahToHome / _batteryCapacity)), bar.Top, bar.Height / 2, bar.Height, 0, 360);
+                etemp.DrawString("H", new Font(this.Font, FontStyle.Bold), new SolidBrush(Color.Black), (bar.X + bar.Width * (mahToHome / _batteryCapacity)) - 2, bar.Bottom - FontHeight);
+
+
+                e.Graphics.DrawImageUnscaled(buffer, 0, 0);
+            }
+
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            //Resize the buffer to the actual control size
+            base.OnResize(e);
+            if (this.Width == 0 || this.Height == 0)
+                return;
+            buffer = new Bitmap(this.Width, this.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        }
+    }
+}
